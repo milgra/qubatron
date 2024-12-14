206478b76e338bdf1e439d5d2ea4e7113f97e28a
Milan Toth <milgra@milgra.pc>
2024-12-14 23:28:50 +0100

    zombie added

--- src/qubatron/main.c
+++ src/qubatron/main.c
@@ -425,9 +425,35 @@ void main_init()
 
     char plypath[PATH_MAX];
 
-    snprintf(plypath, PATH_MAX, "%s../abandoned2.ply", base_path);
+    snprintf(plypath, PATH_MAX, "%s../abandoned1.ply", base_path);
 
-    p_ply ply = ply_open(plypath, NULL, 0, NULL);
+    p_ply ply;
+
+    ply = ply_open(plypath, NULL, 0, NULL);
+
+    if (!ply) mt_log_debug("cannot read %s", plypath);
+    if (!ply_read_header(ply)) mt_log_debug("cannot read ply header");
+
+    nvertices = ply_set_read_cb(ply, "vertex", "x", vertex_cb, NULL, 0);
+    ply_set_read_cb(ply, "vertex", "y", vertex_cb, NULL, 0);
+    ply_set_read_cb(ply, "vertex", "z", vertex_cb, NULL, 1);
+    ply_set_read_cb(ply, "vertex", "red", vertex_cb, NULL, 0);
+    ply_set_read_cb(ply, "vertex", "green", vertex_cb, NULL, 0);
+    ply_set_read_cb(ply, "vertex", "blue", vertex_cb, NULL, 1);
+    ply_set_read_cb(ply, "vertex", "nx", vertex_cb, NULL, 0);
+    ply_set_read_cb(ply, "vertex", "ny", vertex_cb, NULL, 0);
+    ply_set_read_cb(ply, "vertex", "nz", vertex_cb, NULL, 1);
+    mt_log_debug("cloud point count : %lu", nvertices);
+    if (!ply_read(ply)) return;
+    ply_close(ply);
+
+    snprintf(plypath, PATH_MAX, "%s../zombie.ply", base_path);
+
+    ply = ply_open(plypath, NULL, 0, NULL);
+
+    offset.x = -270.0;
+    offset.y = -600.0;
+    offset.z = -10.0;
 
     if (!ply) mt_log_debug("cannot read %s", plypath);
     if (!ply_read_header(ply)) mt_log_debug("cannot read ply header");
